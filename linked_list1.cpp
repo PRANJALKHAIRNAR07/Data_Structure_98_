8103396e995ebc46cd9bfe27fd275e6f713621fe
PRANJALKHAIRNAR07 <pranjalkhairnar318@gmail.com>
2025-09-25 15:44:39 +0530

    Add files via upload

---
+++ linked_list1.cpp
@@ -0,0 +1,78 @@
+/*PROBLEM STATEMENT:
+ Simple Task Scheduler:
+ Write a program that implements a simple task scheduler using a singly linked list. Each
+ node in the linked list represents a task with its priority and execution time. Tasks are 
+ scheduled based on their priority, with higher priority tasks being executed first.
+*/ 
+#include <iostream>
+using namespace std;
+class Node {
+public:
+    string task_name;
+    int priority;
+    int exe_time;
+    Node* next;
+    Node(string tn, int p, int e) {
+        task_name = tn;
+        priority = p;
+        exe_time = e;
+        next = NULL;
+    }
+    void display() {
+        cout<<"Task Name: " <<task_name<<endl;
+        cout<<"Priority: "<< priority<<endl;
+        cout<<"Execution Time: "<<exe_time<<" sec"<<endl;
+        cout<<"-----------------------------"<<endl;
+    }
+};
+int main() {
+    Node* header=NULL;
+    Node* prev=NULL;
+    Node* current=NULL;
+    Node* temp=NULL;
+
+    int n;
+    string tn;
+    int p, e;
+    cout<<"How many tasks do you want to add? ";
+    cin>>n;
+
+    for(int i= 0;i<n;i++) {
+        cout<<"\nEnter task name: ";
+        cin>>tn;
+        cout<<"Enter task priority (higher number means higher priority): ";
+        cin>>p;
+        cout<<"Enter execution time (in seconds): ";
+        cin>>e;
+
+        temp=new Node(tn, p, e);
+        if (header == NULL) {
+            header = temp;
+        } 
+        else {
+            if (header->priority < temp->priority) {
+                temp->next = header;
+                header = temp;
+            } 
+            else {
+                prev=header;
+                current = prev->next;
+                while (current != NULL && current->priority >= temp->priority) {
+                    prev = current;
+                    current = current->next;
+                }
+                prev->next = temp;
+                temp->next = current;
+            }
+        }
+    }
+    cout << "\n=====================================\n";
+    cout << "The Priority List of Tasks is as follows:\n";
+    cout << "=====================================\n\n";
+    Node* t = header;
+    while (t != NULL) {
+        t->display();
+        t = t->next;
+    }
+    return 0;
+}
